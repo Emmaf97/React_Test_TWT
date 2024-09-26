af82b1ef961c7f42d96cd76c0d9525c34090e65f
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-09-26 18:10:43 +0100

    path updates

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import api from "../api";
+import api from "./api";
 import { useNavigate } from "react-router-dom";
-import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
-import "../styles/Form.css"
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "./constants";
+import "./styles/Form.css"
 import LoadingIndicator from "./LoadingIndicator";
 
 function Form({ route, method }) {
