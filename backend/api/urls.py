320c9440a3989cd5516126faefc377074d5e194e
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-09-26 13:04:47 +0100

    adding main route urls.py api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.http import JsonResponse
 from . import views
 
-
+def root_view(request):
+    return JsonResponse({'message': 'Welcome to the API'})
 
 urlpatterns = [
+    path('', root_view),
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
     path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
 ]
