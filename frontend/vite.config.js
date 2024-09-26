3a87826cea1de9b9319137d120f7fdb0f6270b74
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-09-26 13:31:33 +0100

    react router fallback

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true,  // This ensures that React Router works correctly
+  }
 })
