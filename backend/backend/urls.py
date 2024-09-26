2087db6e91aba0b15ae89b5aba7a621c8c2b6237
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-09-26 14:57:41 +0100

    adding root path

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,11 +1,16 @@
 
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+def root_view(request):
+    return JsonResponse({'message': 'Welcome to the API'})
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', root_view),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
