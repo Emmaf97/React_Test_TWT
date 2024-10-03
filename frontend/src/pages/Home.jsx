0d1eed0d4e9a0dc719e6b3828d01b4bd9388a906
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-10-03 21:52:09 +0100

    Changing path to api.js

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import api from "../api";
+import api from "../api.js";
 import Note from "../components/Note";
 import "../styles/Home.css";
 import "../components/NavBar";
