b2439802de5786b77a2158acf2ab47f1f3a928d2
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-10-04 20:11:52 +0100

    Debugging backend url connection failure

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,21 +1,14 @@
 
 from django.contrib import admin
-from django.views.generic import TemplateView
-from django.http import JsonResponse
 from django.urls import path, include
 from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-def root_view(request):
-    return JsonResponse({'message': 'Welcome to the API'})
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', root_view),
+    path("admin/", admin.site.urls),
     path("api/user/register/", CreateUserView.as_view(), name="register"),
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
-    path("api/auth/", include("rest_framework.urls")),
+    path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls")),
-    path('', TemplateView.as_view(template_name="index.html"), name='index'),
 ]
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,17 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Layout from "./components/Layout";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import react from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
+import ProtectedRoute from "./components/ProtectedRoute"
 
 function Logout() {
   localStorage.clear()
-  return <Navigate to="/login"/>
+  return <Navigate to="/login" />
 }
 
 function RegisterAndLogout() {
-  //clearing old access tokens to prevent error from occurring
   localStorage.clear()
   return <Register />
 }
@@ -24,7 +19,6 @@ function RegisterAndLogout() {
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
       <Routes>
         <Route
           path="/"
@@ -36,12 +30,11 @@ function App() {
         />
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Logout />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="*" element={<NotFound />} />
-        </Routes>
-        </Layout>
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </BrowserRouter>
   )
 }
 
-export default App;
+export default App
